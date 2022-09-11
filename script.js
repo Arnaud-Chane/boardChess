03689b9264358092b2dfe7214432fc91b83378f7
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2022-09-11 14:15:54 +0200

    Add files via upload
    Creating chess board

---
+++ script.js
@@ -0,0 +1,58 @@
+// Container
+var boardContainer = document.createElement("div");
+boardContainer.classList.add("container");
+document.body.appendChild(boardContainer);
+
+// Variables
+var boardSize = 8;
+var xIdCase = 1;
+var yIdCase = ["A","B","C","D","E","F","G","H"];
+var caseId = "";
+var arrIdCase = [];
+var caseCount = 0;
+
+// Populate
+for(let e=0; e<boardSize; e++){
+  for(let i=0; i<boardSize; i++){
+    var freshNewCase = document.createElement("div");
+    caseId = yIdCase[e] + xIdCase;
+    freshNewCase.id = caseId;
+    freshNewCase.classList.add("boardCase");
+    document.querySelector(".container").appendChild(freshNewCase);
+
+// Keep count
+    caseCount++;
+    var caseObj = {
+      id: freshNewCase.id,
+      caseNumber: caseCount
+    }
+    arrIdCase.push(caseObj);
+    
+// Color Attribution
+//    if(caseCount % boardSize > 1){ 
+        if(caseCount % 2 === 0){
+          freshNewCase.classList.add("blackCase");
+        } else {
+          freshNewCase.classList.add("whiteCase");
+        }
+ /*   } else {
+        if(caseCount % 2 === 0){
+          freshNewCase.classList.add("whiteCase");
+        } else {
+          freshNewCase.classList.add("blackCase");
+        }
+    }
+  */
+    xIdCase++;
+  }
+  xIdCase = 1;
+}
+
+console.log(arrIdCase);
+console.log(caseCount % 2 );
+
+
+
+
+
+
